e37dce6e13cf298394ea1949ed13f65b28328da9
unknown <2019nidhi.bangera@ves.ac.in>
2022-03-27 17:41:58 +0530

    Fetch

--- service-worker.js
+++ service-worker.js
@@ -150,6 +150,20 @@ function setOfCachedUrls(cache) {
   });
 }
 
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) { return r; }
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
+
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
